57b133380a80f5f7fb8a4e2df0c02a59b30139e8
Joe AB <joe.anthonybrown@gmail.com>
2024-11-24 20:22:40 -0600

    fix logo sizing, add responsive css, begin personal info form

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import logoLeft from './assets/logo-components/Colaborate_Logo_Colab.png'
 import logoCenter from './assets/logo-components/Colaborate_Logo_Hands.png'
 import logoRight from './assets/logo-components/Colaborate_Logo_Rate.png'
 import InterviewQuestions from './components/InterviewQuestions'
+import InfoForm from './components/InfoForm'
 
 function App() {
 
@@ -19,19 +20,31 @@ function App() {
 
   return (
     <>
-      <div className='flex'>
-        <span className='w-1/3 align-middle'><img className='object-cover h-50 w-90 pt-7' src={logoLeft}/></span>
-        <span className='w-1/3'>
-          <img src={logoCenter} className="object-cover h-50 w-90 logo pb-2" alt="logo"/>
-        </span>
-        <span className='w-1/3'><img className='object-cover h-50 w-90 pt-14' src={logoRight}/></span>
+      <div className='flex justify-center'>
+        <div className='logo-container flex justify-center'>
+          <span className='w-1/3'><img className='object-cover h-40' src={logoLeft}/></span>
+          <span className='pl-8'>
+            <img src={logoCenter} className="object-cover h-52 logo" alt="logo"/>
+          </span>
+          <span className='w-1/3 h-40'><img className='object-cover h-40' src={logoRight}/></span>
+        </div>
       </div>
-      <div>Welcome to Co-Lab-o-Rate, an online platform where artists participate in collaborative drawing games and monetize their artwork into products. Let us know a bit about you...   </div>
+      <div>Welcome to<br /> 
+      Co-Lab-o-Rate,<br /> 
+      an online platform<br /> 
+      where artists<br /> 
+      participate in<br /> 
+      collaborative drawing games<br /> 
+      and monetize their artwork<br /> 
+      into products.<br /> 
+      <br /><br />
+      Let us know a bit about you...   </div>
       <div className={surveyStarted ? 'invisible h-0' : 'start-button'}>
         <button className='bg-slate-800 text-white p-5 rounded mt-2' onClick={()=>setSurveyStarted(true)}>Get Started!</button>
       </div>
       <div className={!surveyStarted ? 'invisible' : 'survey-container'}>
         <div>
+          <InfoForm/>
           <InterviewQuestions/>
         </div>
       </div>
---
+++ src/components/InfoForm.tsx
@@ -0,0 +1,38 @@
+
+
+function InfoForm(){
+    return(
+        <div className="flex flex-col">
+            <div>
+                <h6>First Name:</h6>
+                <input className="bg-slate-200"></input>
+            </div>
+            <div>
+                <h6>Last Name:</h6>
+                <input className="bg-slate-200"></input>
+            </div>
+            <div>
+                <h6>Age:</h6>
+                <input className="bg-slate-200"></input>
+            </div>
+            <div>
+                <h6>Location:</h6>
+                <input className="bg-slate-200"></input>
+            </div>
+            <div>
+                <h6>Email:</h6>
+                <input className="bg-slate-200"></input>
+            </div>
+            <div>
+                <h6>Phone Number:</h6>
+                <input className="bg-slate-200"></input>
+            </div>
+            <div>
+                <h6>Which best describes you:</h6>
+            </div>
+
+        </div>
+    )
+}
+
+export default InfoForm;
